fe9502b1fd09596ca7a81cdd102bc9a71bb223fb
Chambers <bobhus394@gmail.com>
2022-05-07 21:22:25 +0800

    更新manifest方式

--- vite.config.js
+++ vite.config.js
@@ -10,26 +10,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        start_url: '/',
-        name: '\u8766\u82f1\u96c4',
-        short_name: '\u8766\u82f1\u96c4',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ],
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone'
-      }
+      manifest: false
     })
   ],
   resolve: {
